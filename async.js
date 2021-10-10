1643849475831905f9a4558e9b6d5e075b3f771e
Mgo <mgotm13@gmail.com>
2021-10-10 22:41:53 +0400

    async file and with some request code that uses OS Async helpers

--- async.js
+++ async.js
@@ -2,13 +2,25 @@ const https = require('https');
 
 const start = Date.now();
 
-https.request('https://www.google.com', function(res) {
-    
-    res.on('data',function(){
 
-    });
+function doRequest() {
+    https.request('https://www.google.com', function(res) {
+   
+        res.on('data',function(){
 
-    res.on('end',function(){
-        console.log(Date.now() - start);
-    });
-}).end();
+        });
+
+        res.on('end',function(){
+            console.log(Date.now() - start);
+        });
+
+    }).end();
+}
+
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
