cf11736549510cc6719ccea67e1573b957375c88
Mgo <mgotm13@gmail.com>
2021-10-11 23:55:37 +0400

    minor modification in the express server

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require("express");
 
 if (cluster.isMaster) {
     cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
 } else {
+    // child mode
+
     const express = require('express');
     const app = express();
 
@@ -14,13 +19,16 @@ if (cluster.isMaster) {
      * */
     function doWorkOnCpu(duration) {
         const start = Date.now()
-        while (Date.now() - start < duration) {
-        }
+        while (Date.now() - start < duration) {}
     }
 
     app.get('/', function (req, res) {
         doWorkOnCpu(5000);
         res.send('Hi there');
+    });
+
+    app.get('/fast', function (req, res) {
+        res.send('Fast route');
     });
 
     app.listen(8080);
