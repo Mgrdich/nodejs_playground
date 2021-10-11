ffe75ae3902ed173f41f7807b928ec9dd9e6de6d
Mgo <mgotm13@gmail.com>
2021-10-11 23:26:37 +0400

    minor modification in the express server

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 
+/***
+ * @description overclocking the event loop
+ * Thus blocking the server
+ * @param duration {Number}
+ * */
+function doWorkOnCpu(duration) {
+    const start = Date.now()
+    while(Date.now() - start < duration) {}
+}
+
 app.get('/', function (req, res) {
+    doWorkOnCpu(5000);
     res.send('Hi there');
 });
 
