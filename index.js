737c7917588c149289f62976add3871ebb3192c6
Mgo <mgotm13@gmail.com>
2021-10-11 23:38:11 +0400

    minor modification in the express server

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
+const express = require("express");
 
-/***
- * @description overclocking the event loop
- * Thus blocking the server
- * @param duration {Number}
- * */
-function doWorkOnCpu(duration) {
-    const start = Date.now()
-    while(Date.now() - start < duration) {}
+if (cluster.isMaster) {
+    cluster.fork();
+} else {
+    const express = require('express');
+    const app = express();
+
+    /***
+     * @description overclocking the event loop
+     * Thus blocking the server
+     * @param duration {Number}
+     * */
+    function doWorkOnCpu(duration) {
+        const start = Date.now()
+        while (Date.now() - start < duration) {
+        }
+    }
+
+    app.get('/', function (req, res) {
+        doWorkOnCpu(5000);
+        res.send('Hi there');
+    });
+
+    app.listen(8080);
 }
 
-app.get('/', function (req, res) {
-    doWorkOnCpu(5000);
-    res.send('Hi there');
-});
 
-app.listen(8080);
 
